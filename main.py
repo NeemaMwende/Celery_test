ec127a93c4e01e3c6da7968b95fe6b72cbd4550b
NeemaMwende <gracepete12@gmail.com>
2026-03-18 01:21:10 +0300

    feat: math tool intergration

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from langchain_core.tools import tool 
 from langchain_ollama import ChatOllama
-from langgraph.prebuilt import create_react_agent
-import os 
-from dotenv import load_dotenv
+#from langgraph.prebuilt import create_react_agent
+from langchain.agents import create_agent
 
-load_dotenv()
+# Define tool
+# @tool
+# def multiply(a: int, b: int) -> int:
+#     """Multiply two numbers"""
+#     return a * b
 
-model = ChatOllama(
-    model="phi3:mini",
-    google_api_key=os.getenv("GOOGLE_API_KEY")
-)
+@tool
+def calculator(expression: str) -> float:
+    """Evaluate a mathematical expression"""
+    return eval(expression)
 
-tools = []  
+model = ChatOllama(model="qwen3:1.7b")
 
-agent = create_react_agent(model, tools)
+tools = [calculator]
+
+# agent = create_react_agent(model, tools) 
+agent = create_agent(model, tools)
 
 query = "what is (2+8) multiplied by 9?"
 response = agent.invoke({"messages": [("human", query)]})
